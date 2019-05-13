101b6cf49ee761f8245f210e31124e4da02a7b92
ashwinsajiv <37055705+ashwinsajiv@users.noreply.github.com>
2019-05-14 00:27:30 +1000

    Add files via upload

--- Volumo/Volumo/MeasureViewController.swift
+++ Volumo/Volumo/MeasureViewController.swift
@@ -7,20 +7,76 @@
 //
 
 import UIKit
+import ARKit
 
-class MeasureViewController: UIViewController {
-    @IBOutlet weak var tempLabel: UILabel!
+class MeasureViewController: UIViewController, ARSCNViewDelegate {
+    @IBOutlet weak var sceneView: ARSCNView!
+    @IBOutlet weak var distance: UILabel!
+    var startingPosition: SCNNode?
+    let configuration = ARWorldTrackingConfiguration()
     var tempText = ""
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.sceneView.session.run(configuration)
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+        self.sceneView.delegate = self
         // Do any additional setup after loading the view.
-        tempLabel.text = tempText
-        
+    }
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        guard let sceneView = sender.view as? ARSCNView else {return}
+        guard let currentFrame = sceneView.session.currentFrame else {return}
+        if self.startingPosition != nil {
+            self.startingPosition?.removeFromParentNode()
+            self.startingPosition = nil
+            return
+        }
+        let camera = currentFrame.camera
+        let transform = camera.transform
+        //var translationMatrix = matrix_identity_float4x4
+        //translationMatrix.columns.3.z = -0.1
+        //let modifiedMatrix = simd_mul(transform, translationMatrix)
+        let sphere = SCNNode(geometry: SCNSphere(radius: 0.005))
+        sphere.geometry?.firstMaterial?.diffuse.contents = UIColor.yellow
+        sphere.simdTransform = transform
+        self.sceneView.scene.rootNode.addChildNode(sphere)
+        self.startingPosition = sphere
+    }
+    @IBAction func Measure(_ sender: UIButton) {
+        if !(tempText == "sphere" || tempText == "cube"){
+        performSegue(withIdentifier: "measureToMeasure", sender: self)
+        }
+        else {
+            performSegue(withIdentifier: "measure1ToResults", sender: self)
+        }
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if(segue.identifier == "measureToMeasure"){
+            let vc = segue.destination as! SecondMeasureViewController
+            vc.tempText = self.tempText
+        }
+        else if (segue.identifier == "measure1ToResults"){
+            let vc = segue.destination as! ResultsViewController
+            vc.tempText = self.tempText
+        }
+    }
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        guard let startingPosition = self.startingPosition else {return}
+        guard let pointOfView = self.sceneView.pointOfView else {return}
+        let transform = pointOfView.transform
+        let location = SCNVector3(transform.m41, transform.m42, transform.m43)
+        let xDistance = location.x - startingPosition.position.x
+        let yDistance = location.y - startingPosition.position.y
+        let zDistance = location.z - startingPosition.position.z
+        DispatchQueue.main.async {
+            self.distance.text = String(format: "%.2f", self.distanceTravelled(x: xDistance, y: yDistance, z: zDistance)) + "m"
+        }
+    }
     
-    @IBAction func Measure(_ sender: UIButton) {
-        performSegue(withIdentifier: "measureToMeasure", sender: self)
+    func distanceTravelled(x: Float, y: Float, z: Float) -> Float {
+        return (sqrtf(x*x + y*y + z*z))
     }
     
     /*
--- Volumo/Volumo/SecondMeasureViewController.swift
+++ Volumo/Volumo/SecondMeasureViewController.swift
@@ -7,19 +7,65 @@
 //
 
 import UIKit
+import ARKit
 
-class SecondMeasureViewController: UIViewController {
-
+class SecondMeasureViewController: UIViewController, ARSCNViewDelegate {
+    var tempText = ""
+    @IBOutlet weak var sceneView: ARSCNView!
+    @IBOutlet weak var distance: UILabel!
+    var startingPosition: SCNNode?
+    let configuration = ARWorldTrackingConfiguration()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.sceneView.session.run(configuration)
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+        self.sceneView.delegate = self
+        // Do any additional setup after loading the view.
+    }
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        guard let sceneView = sender.view as? ARSCNView else {return}
+        guard let currentFrame = sceneView.session.currentFrame else {return}
+        if self.startingPosition != nil {
+            self.startingPosition?.removeFromParentNode()
+            self.startingPosition = nil
+            return
+        }
+        let camera = currentFrame.camera
+        let transform = camera.transform
+        //var translationMatrix = matrix_identity_float4x4
+        //translationMatrix.columns.3.z = -0.1
+        //let modifiedMatrix = simd_mul(transform, translationMatrix)
+        let sphere = SCNNode(geometry: SCNSphere(radius: 0.005))
+        sphere.geometry?.firstMaterial?.diffuse.contents = UIColor.yellow
+        sphere.simdTransform = transform
+        self.sceneView.scene.rootNode.addChildNode(sphere)
+        self.startingPosition = sphere
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let vc = segue.destination as! ResultsViewController
+        vc.tempText = tempText
+    }
     @IBAction func measureNext(_ sender: UIButton) {
         performSegue(withIdentifier: "measure2ToResults", sender: self)
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        guard let startingPosition = self.startingPosition else {return}
+        guard let pointOfView = self.sceneView.pointOfView else {return}
+        let transform = pointOfView.transform
+        let location = SCNVector3(transform.m41, transform.m42, transform.m43)
+        let xDistance = location.x - startingPosition.position.x
+        let yDistance = location.y - startingPosition.position.y
+        let zDistance = location.z - startingPosition.position.z
+        DispatchQueue.main.async {
+            self.distance.text = String(format: "%.2f", self.distanceTravelled(x: xDistance, y: yDistance, z: zDistance)) + "m"
+        }
     }
     
-
+    func distanceTravelled(x: Float, y: Float, z: Float) -> Float {
+        return (sqrtf(x*x + y*y + z*z))
+    }
     /*
     // MARK: - Navigation
 
